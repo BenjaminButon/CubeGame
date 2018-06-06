4a4fa69c79998079aed745407f04f3c095f21e6c
BenjaminButon <Buratino1324>
2018-06-06 10:46:15 +0300

    add cube

---
+++ CubeGame/Cube.swift
@@ -0,0 +1,26 @@
+//
+//  Cube.swift
+//  CubeGame
+//
+//  Created by Benko Ostap on 06.06.18.
+//  Copyright © 2018 Ostap Benko. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+
+let size: CGFloat = 1.0
+let color = UIColor.green
+
+class Cube{
+    
+    static func spawnCubeAt(_ position: SCNVector3) -> SCNNode{
+        let cubeGeometry = SCNBox(width: size, height: size, length: size, chamferRadius: 0.0)
+        cubeGeometry.materials.first?.diffuse.contents = color
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        cubeNode.position = position
+        cubeNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        return cubeNode
+    }
+}
